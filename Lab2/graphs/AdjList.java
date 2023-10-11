31117cc333403b1deb0e1109752b526342dd7044
Zhiqin99 <110769896+Zhiqin99@users.noreply.github.com>
2023-10-11 22:13:34 +0800

    Update AdjList.java

--- Lab2/graphs/AdjList.java
+++ Lab2/graphs/AdjList.java
@@ -1,6 +1,5 @@
 /**
  * AdjList
- */
 public class AdjList extends Graph{
 
     public AdjList(int v){
@@ -8,3 +7,33 @@ public class AdjList extends Graph{
         //add more stuff
     }
 }
+*/
+import java.util.*;
+
+public class DijkstraMinHeap {
+	public static void dijkstra(int V, ArrayList<ArrayList<Node>> graph, int source) {
+		int[] dist = new int[V];
+
+		for (int i = 0; i < V; i++)
+			dist[i] = Integer.MAX_VALUE;
+
+		dist[source] = 0;
+
+		PriorityQueue<Node> minHeap = new PriorityQueue<>((v1, v2) -> v1.getCost() - v2.getCost());
+		minHeap.add(new Node(source, 0));
+
+		while (minHeap.size() > 0) {
+			Node current = minHeap.poll();
+
+			for (Node node : graph.get(current.getVertex())) {
+				if (dist[current.getVertex()] + node.getCost() < dist[node.getVertex()]) {
+					dist[node.getVertex()] = dist[current.getVertex()] + node.getCost();
+					minHeap.add(new Node(node.getVertex(), dist[node.getVertex()]));
+				}
+			}
+		}
+
+		for (int j = 0; j < V; j++)
+			System.out.println("Vertex: " + j + " Minimum Distance from Source: " + dist[j]);
+
+	}
