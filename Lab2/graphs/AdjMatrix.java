e66a3873ea6909be1f127e4e3c0b0dbfb2f437d7
CelineChow <67944114+CelineChow@users.noreply.github.com>
2023-10-12 01:23:34 +0800

    formatting and get matrix function

--- Lab2/graphs/AdjMatrix.java
+++ Lab2/graphs/AdjMatrix.java
@@ -6,51 +6,51 @@ public class AdjMatrix extends Graph {
     private int matrix[][];
 
     // constructor
-    public AdjMatrix(int v){
+    public AdjMatrix(int v) {
         super(v);
         this.matrix = new int[v][v];
     }
 
-    //methods
+    // methods
     @Override
-    public void addEdge(int start, int end, int weight){
+    public void addEdge(int start, int end, int weight) {
         matrix[start][end] = weight;
         this.incrementE();
-        System.out.println("Edge between "+start+" and "+end+" with weight "+weight+" added");
+        System.out.println("Edge between " + start + " and " + end + " with weight " + weight + " added");
     }
 
     @Override
-    public void printGraph(){
-        for(int i = 0; i < this.getV(); i++){
-            if(i == 0){
+    public void printGraph() {
+        for (int i = 0; i < this.getV(); i++) {
+            if (i == 0) {
                 System.out.print("   ");
-                for(int j = 0; j < this.getV(); j++){
+                for (int j = 0; j < this.getV(); j++) {
                     // System.out.print((j+1)+"\t");
-                    System.out.printf("%4s",(j+1));
+                    System.out.printf("%4s", (j + 1));
                 }
-                printLine(this.getV()*5);
+                printLine(this.getV() * 5);
             }
-            System.out.printf("%2s |", i+1);
-            for(int j = 0; j < this.getV(); j++){
+            System.out.printf("%2s |", i + 1);
+            for (int j = 0; j < this.getV(); j++) {
                 // System.out.print(matrix[i][j]+"\t");
                 System.out.printf("%4s", matrix[i][j]);
-                
+
             }
             System.out.println("");
         }
     }
 
     // create random graph with e edges, weights 1 to 10
-    public void randomGraph(int e){
+    public void randomGraph(int e) {
         int start, end, weight;
 
-        for(int i = 0; i < e; i++){
+        for (int i = 0; i < e; i++) {
             start = (int) Math.floor(Math.random() * getV());
             end = (int) Math.floor(Math.random() * getV());
 
-            if(start == end){ //make sure start doesnt == end
+            if (start == end) { // make sure start doesnt == end
                 end++;
-                if(end > getV()-1){
+                if (end > getV() - 1) {
                     end = 0;
                 }
             }
@@ -60,55 +60,55 @@ public class AdjMatrix extends Graph {
         }
     }
 
-    private void printLine(int len){
+    private void printLine(int len) {
         System.out.print("\n");
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             System.out.print("-");
         }
         System.out.print("\n");
     }
 
-    public int [] adjMatDijkstra(int [] [] matrix , int source){
-	    	   boolean visited [] = new boolean[super.getV()];
-	    	   int dist [] = new int [super.getV()];
-	    	   
-	    	   for(int i = 0 ; i < super.getV() ; i++) {
-	    		   visited[i] = false;
-	    		   dist[i] = Integer.MAX_VALUE;
-	    	   }
-	    	   dist[source] = 0;
-	    	   
-	    	   
-	    	   for(int i = 0 ; i < super.getV() ; i++) {
-	    		   int min = nextMin(dist , visited);
-	    		   
-	    		   visited[min] = true;
-	    		   
-	    		   for(int j = 0 ; j < super.getV() ; j++) {
-	    			   if(matrix[min][j] > 0 && dist[j] > matrix[min][j] + dist[min]){
-	    				   dist[j] = matrix[min][j] + dist[min];
-	    			   }
-	    		   }
-	    	   }
-	    	   
-	    	   
-	    	return dist;
-	       }
-	        
-	       //helper function to get next min
-	private int nextMin(int [] distance , boolean [] visited){
-	    	int min = Integer.MAX_VALUE;
-	    	int min_index = -1;
-	    	   
-	    	for(int i = 0 ; i < super.getV() ; i++) {
-	    		if(visited[i] == false && distance[i] < min) {
-	    			min = distance[i];
-	    			min_index = i;
-	    		   }
-	    	   }
-	    return min_index;
-	  }
-	        
-	        
+    public int[] adjMatDijkstra(int[][] matrix, int source) {
+        boolean visited[] = new boolean[super.getV()];
+        int dist[] = new int[super.getV()];
+
+        for (int i = 0; i < super.getV(); i++) {
+            visited[i] = false;
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[source] = 0;
+
+        for (int i = 0; i < super.getV(); i++) {
+            int min = nextMin(dist, visited);
+
+            visited[min] = true;
+
+            for (int j = 0; j < super.getV(); j++) {
+                if (matrix[min][j] > 0 && dist[j] > matrix[min][j] + dist[min]) {
+                    dist[j] = matrix[min][j] + dist[min];
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    // helper function to get next min
+    private int nextMin(int[] distance, boolean[] visited) {
+        int min = Integer.MAX_VALUE;
+        int min_index = -1;
+
+        for (int i = 0; i < super.getV(); i++) {
+            if (visited[i] == false && distance[i] < min) {
+                min = distance[i];
+                min_index = i;
+            }
+        }
+        return min_index;
+    }
+
+    public int[][] getMatirx() {
+        return matrix;
+    }
 
 }
