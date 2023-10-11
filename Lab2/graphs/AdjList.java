b787d2db54644483cd65a9100dead561d334c61d
Zhiqin99 <110769896+Zhiqin99@users.noreply.github.com>
2023-10-11 22:38:03 +0800

    Update AdjList.java

--- Lab2/graphs/AdjList.java
+++ Lab2/graphs/AdjList.java
@@ -9,6 +9,50 @@ public class AdjList extends Graph{
 }
 */
 import java.util.*;
+ 
+public class AdjList {
+ 
+    // Function to convert adjacency
+    // list to adjacency matrix
+    static ArrayList<ArrayList<Integer>> convert(int[][] a)
+    {
+        // no of vertices
+        int l = a[0].length;
+        ArrayList<ArrayList<Integer>> adjListArray 
+        = new ArrayList<ArrayList<Integer>>(l);
+ 
+        // Create a new list for each
+        // vertex such that adjacent
+        // nodes can be stored
+        for (int i = 0; i < l; i++) {
+            adjListArray.add(new ArrayList<Integer>());
+        }
+         
+        int i, j;
+        for (i = 0; i < a[0].length; i++) {
+            for (j = 0; j < a.length; j++) {
+                if (a[i][j] != 0) {
+                    adjListArray.get(i).add(j);
+                }
+            }
+        }
+         
+        return adjListArray;
+    }
+     
+    // Function to print the adjacency list
+    static void printArrayList(ArrayList<ArrayList<Integer>> 
+                                                adjListArray)
+    {
+        // Print the adjacency list
+        for (int v = 0; v < adjListArray.size(); v++) {
+            System.out.print(v);
+            for (Integer u : adjListArray.get(v)) {
+                System.out.print(" -> " + u);
+            }
+            System.out.println();
+        }
+    }
 
 public class DijkstraMinHeap {
 	public static void dijkstra(int V, ArrayList<ArrayList<Node>> graph, int source) {
