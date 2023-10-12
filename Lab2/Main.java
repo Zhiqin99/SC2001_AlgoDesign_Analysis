18aa5437dd0259158bc4025485b16b0fdba5c9f0
Zhiqin99 <110769896+Zhiqin99@users.noreply.github.com>
2023-10-12 16:35:48 +0800

    Update Main.java

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -1,18 +1,20 @@
 
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
        
 
         //adjmatrix test
-        AdjMatrix matrix = new AdjMatrix(10);
-        matrix.randomGraph(50);
+        AdjMatrix matrix = new AdjMatrix(4);
+        matrix.randomGraph(4);
         matrix.printGraph();
 
-        AdjList list = new AdjList(10);
-        ArrayList<ArrayList<Integer>> l = AdjList.convert(matrix.getMatirx());
-
+        
+        ArrayList<ArrayList<Node>> l = AdjList.convert(matrix.getMatirx());
         AdjList.printArrayList(l);
+        DijkstraMinHeap.dijkstra(l, 0);
+        matrix.adjMatDijkstra(matrix.getMatirx(), 0);
         
         
        
